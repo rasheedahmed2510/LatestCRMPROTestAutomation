7acd652170e79261ccec579f1d9a0bff91b74660
Rasheed Ahmed <rasheedahmed2510@outlook.com>
2019-09-08 19:25:40 +0530

    Second commit updated maven version

--- src/main/java/com/qa/crm/pages/SignUpPage.java
+++ src/main/java/com/qa/crm/pages/SignUpPage.java
@@ -3,7 +3,6 @@ package com.qa.crm.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import com.qa.crm.base.BaseTest;
 
 public class SignUpPage extends BaseTest {
@@ -11,6 +10,20 @@ public class SignUpPage extends BaseTest {
 	//Object Repository
 	@FindBy(xpath="//div[text()='Select edition above']")
 	private WebElement selectEditionText;
+	
+	@FindBy(xpath="//select[@name='payment_plan_id']")
+	private WebElement selectEditionDropdown;
+	
+	@FindBy(xpath="//input[@name='first_name']")
+	private WebElement firstName;
+	
+	@FindBy(xpath="//input[@placeholder='Last Name']")
+	private WebElement lastName;
+	
+	@FindBy(xpath="//input[@name='email']")
+	private WebElement emailAddress;
+	
+	
 	
 	//Constructor
 	public SignUpPage(){
@@ -21,5 +34,7 @@ public class SignUpPage extends BaseTest {
 	public boolean isSelectEditionTextDisplayed(){
 		return selectEditionText.isDisplayed();
 	}
+	
+	
 	
 }
